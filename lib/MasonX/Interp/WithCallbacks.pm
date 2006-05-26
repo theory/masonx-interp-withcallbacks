05af39bf856129261a79151d896391e6b95cd5fd
David Wheeler <david@justatheory.com>
2006-05-26 23:03:01 +0000

    Incremented version number to 1.16.

--- lib/MasonX/Interp/WithCallbacks.pm
+++ lib/MasonX/Interp/WithCallbacks.pm
@@ -8,7 +8,7 @@ use Params::CallbackRequest;
 
 use vars qw($VERSION @ISA);
 @ISA = qw(HTML::Mason::Interp);
-$VERSION = '1.15';
+$VERSION = '1.16';
 
 Params::Validate::validation_options
   ( on_fail => sub { HTML::Mason::Exception::Params->throw( join '', @_ ) } );
